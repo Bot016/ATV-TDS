45f2a9390c31cef7d8874cd29aab74d9b3448282
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-08-22 10:26:31 -0300

    Adicionar arquivos de projeto.

---
+++ mensagens/Models/message.cs
@@ -0,0 +1,9 @@
+namespace Bot.Models
+{
+    public class Message
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Content { get; set; }
+    }
+}
